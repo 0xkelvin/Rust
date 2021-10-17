abc5c72752c2d2eab1ee58d9690b9f374b4434e4
vietmaiquoc <quocvm1@gmail.com>
2021-10-18 00:27:01 +0800

    [Viet] add notes about memory allocation

--- Ownership/Ownership.rs
+++ Ownership/Ownership.rs
@@ -23,3 +23,36 @@ First, let’s take a look at the ownership rules. Keep these rules in mind as w
 - When the owner goes out of scope, the value will be dropped.
 
  */
+
+ fn main()
+ {
+    // s is not valid here, it’s not yet declared
+     let s = "hello"; // s is valid from this point forward
+     let mut s1 = String::from("tring::from hello"); //This type is allocated on the heap and as such is able to store an amount of text that is unknown to us at compile time
+    // do stuff with s
+     s1.push_str(", world !");  // push_str() appends a literal to a String
+     printlt!("{}", s); // this will print out
+ }// this scope is now over, and s is no longer valid
+
+ /*
+ MEMORY and ALLOCATION
+ In the case of a string literal, we know the contents at compile time, so the text is hardcoded directly into the final executable. 
+ This is why string literals are fast and efficient. 
+ But these properties only come from the string literal’s immutability. 
+ Unfortunately, we can’t put a blob of memory into the binary for each piece of text whose size is unknown at compile time and whose size might change while running the program.
+
+With the String type, in order to support a mutable, growable piece of text, we need to allocate an amount of memory on the heap, unknown at compile time, to hold the contents. 
+This means:
+ - The memory must be requested from the memory allocator at runtime.
+ - We need a way of returning this memory to the allocator when we’re done with our String.
+That first part is done by us: when we call String::from, its implementation requests the memory it needs. This is pretty much universal in programming languages.
+
+However, the second part is different. 
+In languages with a garbage collector (GC), the GC keeps track and cleans up memory that isn’t being used anymore, and we don’t need to think about it. 
+Without a GC, it’s our responsibility to identify when memory is no longer being used and call code to explicitly return it, just as we did to request it.
+ Doing this correctly has historically been a difficult programming problem. 
+ If we forget, we’ll waste memory. If we do it too early, we’ll have an invalid variable. 
+ If we do it twice, that’s a bug too. We need to pair exactly one allocate with exactly one free.
+ 
+ 
+ */
