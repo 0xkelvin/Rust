0f1d8cb3cdcb91535d9a96a7d5e684f195a85bdb
vietmaiquoc <quocvm1@gmail.com>
2022-02-05 23:29:29 +0800

    [Viet] study Function

---
+++ Rust_By_Example/Function/method_syntax.rs
@@ -0,0 +1,26 @@
+#[#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+fn main() {
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    }
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect1.area()
+    );   
+}
