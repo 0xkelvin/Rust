e833b619c9587a518d0754c055cd1e85cade3aea
Kelvin <quocvm1@gmail.com>
2022-04-18 08:56:44 +0800

    try to understand scoping rule

--- Rust_By_Example/Scoping_rules/mutability.rs
+++ Rust_By_Example/Scoping_rules/mutability.rs
@@ -1,19 +1,45 @@
+#[allow(dead_code)]
+#[#[derive(Clone, Copy)]]
+struct Book {
+    // &'static str is a reference to a string allocated in read only memory
+    author: &'static str,
+    title: &'static str,
+    year: u32,
+}
+
+// this function takes a reference to a book
+fn borrow_book(book: &Book) {
+    println!("i immutable borrowd {} - {} edition", book.title, book.year);
+}
+
+// This function takes a reference to a mutable book and changes `year` to 2014
+fn new_edition(book: &mut Book) {
+    book.year = 2014;
+    println!("I mutably borrowed {} - {} edition", book.title, book.year);
+}
+
 fn main() {
-    let immutable_box = Box::new(5u32);
+    //create an immutable Book named 'immutabook'
+    let immutabook = Book {
+                // string literals have type `&'static str`
+                author: "Douglas Hofstadter",
+                title: "Gödel, Escher, Bach",
+                year: 1979,
+    };
 
-    println!("immutable_box contains {}", immutable_box);
+    // Create a mutable copy of `immutabook` and call it `mutabook`
+    let mut mutabook = immutabook;
+    
+    // Immutably borrow an immutable object
+    borrow_book(&immutabook);
 
-    // Mutability error
-    //*immutable_box = 4;
-
-    // *Move* the box, changing the ownership (and mutability)   
-
-    let mut mutable_box = immutable_box;
-
-    println!("mutable_box contains {}", mutable_box);
-
-    // modify the contents of the box
-    *mutable_box = 4;
-
-    println!("mutable_box now contains {}", mutable_box);
+    // Immutably borrow a mutable object
+    borrow_book(&mutabook);
+    
+    // Borrow a mutable object as mutable
+    new_edition(&mut mutabook);
+    
+    // Error! Cannot borrow an immutable object as mutable
+    new_edition(&mut immutabook);
+    // FIXME ^ Comment out this line
 }
